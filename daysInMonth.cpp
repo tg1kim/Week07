d7fe7640a6bb8cd9d32be4772d94c3341d32c775
tg1kim <ktg@kangnam.ac.kr>
2024-10-11 15:50:45 +0900

    Add files via upload

---
+++ daysInMonth.cpp
@@ -0,0 +1,18 @@
+// 달의 일수를 계산하는 프로그램
+#include <stdio.h>
+
+#pragma warning(disable: 4996 4326 6031)
+
+void main() 
+{
+	int month, days;
+	
+	"달을 입력하시오: "
+	month에 입력한다
+
+	switch
+	28일
+	30일
+	그 외
+	"%d월의 일수는 %d입니다.\n"
+}
---
+++ goto.cpp
@@ -0,0 +1,18 @@
+// 구구단 출력 프로그램
+
+#include <stdio.h>
+
+#pragma warning(disable: 4996 4326 6031)
+
+void main() 
+{
+	int i = ?
+
+loop:
+	"%d * %d = %d\n"
+	증가하여 10이면
+		end로 간다
+	loop로 간다
+end:
+	;
+}
